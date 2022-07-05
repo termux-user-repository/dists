8dd79b166c14cfcbda6838a79c696bc2d7f5afe3
kcubeterm <kcubeterm@gmail.com>
2022-07-05 13:30:19 +0530

    Updated

--- scripts/remote_handler.sh
+++ scripts/remote_handler.sh
@@ -67,15 +67,18 @@ remove_redundent_deb() {
     done
 }
 
-remove_deb_from_temp_gh() {
+remove_archive_from_temp_gh() {
     echo "removing temporay archives"
     json=$(mktemp /tmp/repo.XXXXXXX)
-    temp_deb=$(mktemp /tmp/remote.XXXXXXX)
+    tur_repo='tur'
     gh api  \
 	    -H "Accept: application/vnd.github.v3+json" \
-	     https://api.github.com/repos/${owner}/${repo}/releases > $api_json
+	     https://api.github.com/repos/${owner}/$tur_repo/releases > $json
 
-    jq -r '.[] | select(.tag_name=="0.1") | .assets[].name' $api_json > $remote_deb_list
+    for temp in $(jq -r '.[] | select(.tag_name=="0.1") | .assets[].name' $json);do
+        gh release delete-asset -R github.com/$owner/$tur_repo $tag $deb -y
+        echo "temp from tur $temp removed!!"
+    done
 }
 commit() {
     pushd $(dirname $BASE_DIR)
