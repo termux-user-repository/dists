e720b4d5e2a553edb5e8d1f0d0dacf0927eb35bc
kcubeterm <kcubeterm@gmail.com>
2022-07-05 13:10:15 +0530

    Updated

--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -10,8 +10,8 @@ gh api -H "Accept: application/vnd.github.v3+json"  https://api.github.com/repos
 
 if jq -r '.[] | select(.tag_name=="0.1") | .assets[].name' $asset_json | grep -G ".tar$";then
     echo "Jai ho. we got some unprocessed debfile."
-    if ./$base_dir/dists_handler.sh;then
-        ./$base_dir/remote_handler.sh
+    if $base_dir/dist_handler.sh;then
+        $base_dir/remote_handler.sh
     else
         exit 1
     fi
